0a581cb1e5410bcde4b42d3cda0ac008741659e6
Ilana Shapiro <ilanashapiro@wl-192-160.wpa.claremont.edu>
2020-02-23 12:11:36 -0800

    feat(FirebaseUI): working on adding FirebaseUI to create login UI as the initial screen of the app

--- SafeWalk/AppDelegate.swift
+++ SafeWalk/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- SafeWalk/SceneDelegate.swift
+++ SafeWalk/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,7 +18,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+
+                
+                
+        let providers: [FUIAuthProvider] = [
+          FUIEmailAuth(),
+        ]
+        authUI?.providers = providers
+                
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        //                   let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                           // Present the auth view controller and then implement the sign in callback.
+                
+        let authViewController = authUI!.authViewController()
+        //                authViewController.set
+        //                mainStoryboard.instantiateViewController(identifier:)
+        window?.windowScene = windowScene
+       window?.rootViewController = authViewController
+
+       window?.makeKeyAndVisible()
+                
+                           
+                
+//                        let window = UIWindow(frame: UIScreen.main.bounds)
+//                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//
+//                        window.rootViewController = authViewController
+//                        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
