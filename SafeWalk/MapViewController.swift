58825831b8b92003f6d0fd4259f1a84645861335
jennabrandt <jenna.brandt@pomona.edu>
2020-02-23 11:10:54 -0800

    feat(mapspage): added buttons to go and from maps page

---
+++ SafeWalk/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  SafeWalk
+//
+//  Created by Jenna Brandt on 2/22/20.
+//  Copyright © 2020 Ilana Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
