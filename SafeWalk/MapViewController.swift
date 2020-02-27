1f5659072a9eb73139b6dc4b7bbd2441b1c25b57
Ilana Shapiro <ilanashapiro@wl-192-160.wpa.claremont.edu>
2020-02-27 00:01:40 -0800

    feat(logout): user can logout of the app from the Maps view controller and return to the Welcome screen (navigation stack is cleared). Storyboard is cleaned up somewhat

--- SafeWalk/MapViewController.swift
+++ SafeWalk/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import Firebase
 
 class MapViewController: UIViewController {
     //@IBOutlet weak var helloLabel: UILabel!
@@ -29,20 +30,40 @@ class MapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // logs out the user and returns to the welcome screen, clearing the navigation stack
+    @objc func logout() {
+        do {
+            try Auth.auth().signOut()
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let welcomeViewController = storyBoard.instantiateViewController(withIdentifier: "WelcomeViewController")
+            let window = self.view.window
+            window?.rootViewController = welcomeViewController
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
   
-  override func loadView() {
-    // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
-    let camera = GMSCameraPosition.camera(withLatitude: 34.1, longitude: -117.7, zoom: 8.0)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    view = mapView
+    // custom loading of the view to display Google Maps
+    override func loadView() {
+        super.loadView()
+        
+        //     Create a GMSCameraPosition that tells the map to display the
+        //     coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 34.1, longitude: -117.7, zoom: 8.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let logoutButton = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logout))
+        self.navigationItem.leftBarButtonItem = logoutButton
 
-    // Creates a marker in the center of the map.
-    let marker = GMSMarker()
-    marker.position = CLLocationCoordinate2D(latitude: 34.1, longitude: -117.7)
-    marker.title = "Claremont"
-    marker.snippet = "California"
-    marker.map = mapView
-  }
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 34.1, longitude: -117.7)
+        marker.title = "Claremont"
+        marker.snippet = "California"
+        marker.map = mapView
+    }
 
 }
