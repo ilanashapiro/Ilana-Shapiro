928c8e534bc25782c32658f69f3be6d301f8e221
jennabrandt <jenna.brandt@pomona.edu>
2020-03-10 13:44:03 -0700

    style(directionspage): added comments, cleaned up code

--- SafeWalk/DirectionsViewController.swift
+++ SafeWalk/DirectionsViewController.swift
@@ -11,33 +11,30 @@ import GoogleMaps
 import GooglePlaces
 import Firebase
 
+// view controller for directions page and emergency contact calling
 class DirectionsViewController: UIViewController {
     
     // goes back to choose path maps page
-    
     @objc func backButtonTapped() {
         performSegue(withIdentifier: "backSegue", sender: self)
         
     }
 
+    // goes to profile page
     @objc func profileButtonTapped() {
         performSegue(withIdentifier: "profileSegue", sender: self)
     }
 
-    // custom loading of the view to display Google Maps
+    // loading the segue
     override func loadView() {
         super.loadView()
         
-//        definesPresentationContext = true
-//
-//        // navigation buttons on the map view controller
-//        let backButton = UIBarButtonItem(title: "Back", style: UIBarButtonItem.Style.plain, target: self, action: #selector(backButtonTapped))
         let profileButton = UIBarButtonItem(title: "Go to Profile", style: UIBarButtonItem.Style.plain, target: self, action:#selector(profileButtonTapped))
-//        self.navigationItem.leftBarButtonItem = backButton
+
         self.navigationItem.rightBarButtonItem = profileButton
     }
     
-    
+    // button to call 911
     @IBAction func call911(_ sender: Any) {
         let url: NSURL = URL(string: "TEL://911")! as NSURL
         UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
